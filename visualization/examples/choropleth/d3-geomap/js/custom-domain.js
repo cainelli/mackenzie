2ad1788c15d2e5be845d5400b2214c5e50ae8259
Fernando Cainelli <fernando.cainelli@gmail.com>
2017-05-17 18:34:49 -0300

    added full path in custom-domain

--- visualization/examples/choropleth/d3-geomap/js/custom-domain.js
+++ visualization/examples/choropleth/d3-geomap/js/custom-domain.js
@@ -1,12 +1,12 @@
 var map = d3.geomap.choropleth()
-    .geofile('/d3-geomap/topojson/world/countries.json')
+    .geofile('https://cainelli.github.io/mackenzie/visualization/examples/choropleth/d3-geomap/topojson/world/countries.json')
     .colors(['green','red'])
     .column('1800')
     .domain([0, 1])
     .legend(false)
     .unitId('Country');
 
-d3.csv('/data/custom-domain.csv', function(error, data) {
+d3.csv('https://cainelli.github.io/mackenzie/visualization/examples/choropleth/data/custom-domain.csv', function(error, data) {
     d3.select('#map')
         .datum(data)
         .call(map.draw, map);
